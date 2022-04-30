8f728c1f76772bc7a2835490acc1a7dfd932fcb0
jamesbeegen <77640214+jamesbeegen@users.noreply.github.com>
2022-04-29 22:06:29 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 sudo rm /usr/bin/manpac
 sudo rm -rf /opt/manpac
+sudo pacman -S python-virtualenv --noconfirm
 virtualenv manpacqt
 source /opt/manpac/manpacqt/bin/activate
 pip3 --no-input install PyQt5 
 deactivate
 sudo cp -r ../manpac  /opt/manpac
 rm -rf ../manpac
-sudo pacman -S python-virtualenv --noconfirm
 cd /opt/manpac
 sudo chmod +x install.sh
 sudo chmod +x manpac.sh
